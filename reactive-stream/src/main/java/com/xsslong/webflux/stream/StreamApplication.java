6a252fa6270cf5eb86a5f384a8a740a19ef1162b
xutong <xutong@xgd.com>
2024-01-17 17:31:45 +0800

    update

--- reactive-stream/src/main/java/com/xsslong/webflux/stream/StreamApplication.java
+++ reactive-stream/src/main/java/com/xsslong/webflux/stream/StreamApplication.java
@@ -2,7 +2,6 @@ package com.xsslong.webflux.stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 public class StreamApplication {
